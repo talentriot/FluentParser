16542c1439e59be05def3877e6856954f55ff5b4
Erik Johnson <erik@talentriot.com>
2014-02-14 15:41:20 -0700

    populated domain object

--- DiceParser/DiceSearchResult.cs
+++ DiceParser/DiceSearchResult.cs
@@ -8,5 +8,12 @@ namespace DiceParser
 {
     class DiceSearchResult
     {
+        public int TotalSearchResults { get; set; }
+        public List<string> SearchKeywords { get; set; }
+        public int? ZipcodeSearched { get; set; }
+        public string CitySearched { get; set; }
+        public string StateSearched { get; set; }
+        public int SearchRadiusInMiles { get; set; }
+        public IEnumerable<DiceListing> Listings { get; set; }
     }
 }
