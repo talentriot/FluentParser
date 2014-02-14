504c22d34cb8db630a7e68fd497d2216379e3581
Nate Johnson <nate@talentriot.com>
2014-02-14 15:49:44 -0700

    Added base specs and static html file to project.

---
+++ FluentParserSpecs/DiceParserSpecs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using FluentParser;
+using HtmlAgilityPack;
+using NUnit.Framework;
+using DiceParser;
+
+namespace FluentParserSpecs
+{
+    [TestFixture]
+    public class DiceParserSpecs
+    {
+        public static readonly string DicePostingsPagePath = "./Pages/java.html";
+
+        [Test]
+        public void _001_We_should_be_able_to_parse_a_postings_page()
+        {
+            var htmlDocument = DicePostingsPagePath.ReadHtmlFromFile();
+
+            var parser = new DicePostingParser();
+            var parsedPostings = parser.GetAllPostings(htmlDocument);
+        }
+
+        private void GetExpectedNumberOfPostings()
+        {
+        }
+    }
+}
