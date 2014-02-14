02e6f36a660a2e304c9485601ef48c2db029fa8a
Nate Johnson <nate@talentriot.com>
2014-02-14 15:53:23 -0700

    Added basic dice posting parser and object structure.

---
+++ DiceParser/DiceParser/DicePosting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceParser
+{
+    public class DicePosting
+    {
+        public string Id { get; set; }
+        public DateTime DatePosted { get; set; }
+        public string JobTitle { get; set; }
+        public string Company { get; set; }
+        public string Location { get; set; }
+        public string PostingUrl { get; set; }
+    }
+}
---
+++ DiceParser/DiceParser/DicePostingParser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fizzler.Systems.HtmlAgilityPack;
+using FluentParser;
+using HtmlAgilityPack;
+
+namespace DiceParser
+{
+    public class DicePostingParser
+    {
+        public List<DicePosting> GetAllPostings(HtmlDocument DicePage)
+        {
+            var document = DicePage.DocumentNode;
+
+            var postingTable = document.QueryFromSelectorChain("table.summary");
+            var allJobPostings = postingTable.QueryManyFromSelectorChain("tr");
+
+            return allJobPostings.Select(htmlListing => GetPostingFromTableRow(htmlListing)).ToList(); 
+        }
+
+        private static DicePosting GetPostingFromTableRow(HtmlNode htmlPosting)
+        {
+            return null;
+        }
+    }
+}
