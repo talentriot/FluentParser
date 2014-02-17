c9956351e28b06c59d9b23953cdf4f8c1a51cebe
Jon Bullock <jon@talentriot.com>
2014-02-17 14:43:16 -0700

    Added failing Dice Parser Spec.

---
+++ FluentParserSpecs/DiceParserSpecs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace FluentParserSpecs
+{
+    [TestFixture]
+    public class DiceParserSpecs
+    {
+        public static readonly string DiceSearchResultsPage = "./Pages/DiceSearchResultPage.html";
+
+        [Test]
+        public void _001_We_Should_Be_Able_To_Parse_A_Search_Results_Page()
+        {
+            var htmlDocument = DiceSearchResultsPage.ReadHtmlFromFile();
+            throw new NotImplementedException();
+        }
+    }
+}
